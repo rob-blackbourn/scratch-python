97b424d3c3e0aabe7a94be33e33ed7d076cbeb89
Rob Blackbourn <rob.blackbourn@googlemail.com>
2018-09-30 12:45:41 +0100

    Removed unnecessary import

--- contests-graphql/contests/server.py
+++ contests-graphql/contests/server.py
@@ -1,9 +1,6 @@
 from aiohttp import web
-from aiohttp_graphql import GraphQLView
-from graphql.execution.executors.asyncio import AsyncioExecutor
 
 from contests.config import CONFIG
-from contests.schema import schema
 from contests.initialization import initialize
 
 app = web.Application()
