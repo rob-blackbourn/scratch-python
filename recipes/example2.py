c21d380bc08f9a354cb8b3e3692c13fd9ad107a0
Rob Blackbourn <rob.blackbourn@googlemail.com>
2020-01-11 07:11:02 +0000

    Added renaming

--- recipes/example2.py
+++ recipes/example2.py
@@ -5,22 +5,50 @@ from typing import Any, List
 import pkg_resources
 
 
-def descend(folder: Path):
+def descend(root: Path, folder: Path):
     for item in folder.iterdir():
         if item.is_file():
             if item.suffix == '.json':
                 filename = str(item)
                 with open(filename, "rt") as fp:
                     recipe = json.load(fp)
-                ascii_filename = filename.lower().encode(
+                ascii_filename = item.parts[-1].lower().encode(
                     'ascii', 'ignore'
-                ).decode('ascii').replace(' ', '-')
-                print(ascii_filename)
+                ).decode(
+                    'ascii'
+                ).replace(
+                    '_', ''
+                ).replace(
+                    ' ', '-'
+                ).replace(
+                    '--', '-'
+                ).replace(
+                    ',', '-and'
+                ).replace(
+                    '--', '-'
+                )
+                elements = []
+                cuisine = recipe.get('recipeCuisine')
+                if cuisine:
+                    elements.append(cuisine.lo)
+                keywords = recipe.get('keywords')
+                if keywords:
+                    elements += [
+                        keyword.lower().replace(' ', '-')
+                        for keyword in keywords
+                    ]
+                new_folder = root.joinpath(*elements)
+                if not new_folder.exists():
+                    new_folder.mkdir(parents=True)
+                new_item = new_folder.joinpath(ascii_filename)
+                item.rename(new_item)
+                print(new_path)
         elif item.is_dir():
-            descend(item)
+            descend(root, item)
         else:
             raise RuntimeError("Unknown type")
 
 
 if __name__ == "__main__":
-    descend(Path("Recipes"))
+    root = Path("Recipes")
+    descend(root, root)
